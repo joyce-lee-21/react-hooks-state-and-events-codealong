026981191ca1f8594008f909bb7346dc726d6a17
Joyce Lee <joycelee901@gmail.com>
2021-06-09 14:48:13 -0500

    commit tree 8a3fdf593f3e43cd275986286ede135e2352b8fa

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isToggled, setToggled] = useState(false);
+
+  function clickState() {
+    setToggled(!isToggled)
+  }
+  
+  const color = isToggled ? "red" : "white"
+
+  console.log(`click: ${isToggled}`)
+
+  return (
+    <button style={{ background: color }} onClick={clickState}>
+      { isToggled ? "ON" : "OFF" }
+    </button>
+  )
 }
 
 export default Toggle;
